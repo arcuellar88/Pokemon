b4e3ff654befacc9f3f77238a969cea455d3d8f8
Alejandro Rodriguez <a.rodriguez@galapagoagro.co>
2025-09-15 13:29:02 -0500

    feat: main function

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main() {
+int main(){
+
+    string player_name;
+    // Write (output) to console
+    cout << "Enter your name: ";
+
+    // Read (input) from console
+    cin >> player_name;
+
+    cout << "Great Start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
+
 
     return 0;
 }
