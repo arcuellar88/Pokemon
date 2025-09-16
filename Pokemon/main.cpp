e2b6f5838f3a1f8b0262bdcd94bc82c2e8538625
Alejandro Rodriguez <a.rodriguez@galapagoagro.co>
2025-09-16 08:45:00 -0500

    feat: enum Pokemon

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -23,14 +23,26 @@ public:   // access specifier
         cout  << number << ". " << name << endl;
     }
     void selected() const {
-        cout << "You chose " << name << " A " << type << " choice." << endl;;
+        cout << "Professor Oak: You chose " << name << " A " << type << " choice." << endl;;
     }
 
 };
 
+
+enum PokemonChoice {
+    Bulbasaur,
+    Charmander,
+    Squirtle,
+    InvalidChoice,
+};
+
 int main(){
 
+    PokemonChoice chosen_pokemon=InvalidChoice;
+    Pokemon pokemon;
+
     string player_name;
+
     // Write (output) to console
     cout << "Enter your name: ";
 
@@ -41,26 +53,42 @@ int main(){
 
     int pokemon_id;
 
-    Pokemon pokemons[3]={
+    Pokemon pokemons_choices[4]={
         Pokemon( "Bulbasaur (Grass)",1,"wise"),
         Pokemon( "Charmander (Fire)",2,"fiery"),
-        Pokemon( "Squirtle (Water)",3,"cool")
+        Pokemon( "Squirtle (Water)",3,"cool"),
+        Pokemon("Pikachu",4,"surprise")
     };
 
     cout << "Hello! I am Professor Oak. Please choose a POKEMON. Enter the number:" << endl;
 
-    for (Pokemon p : pokemons) {
-        p.selection();
+    for (Pokemon p : pokemons_choices) {
+        if (p.number < 4) {
+            p.selection();
+
+        }
     }
 
-    cin>> pokemon_id;
+    int choice;
+    cin >> choice;
 
-    if (pokemon_id>3 || pokemon_id<1){
-        cout << "Invalid choice. Please restart the game." << endl;
+    if (choice>3 || choice<1){
+        cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you...\n" << endl;
+        pokemon = pokemons_choices[3]; // Default if no valid choice is made
+        cout << "Professor Oak: Just kidding! Let's go with Pikachu, the surprise guest!\n";
     }
     else {
-        pokemons[pokemon_id-1].selected();
+        pokemon=pokemons_choices[choice-1];
+        pokemon.selected();
     }
+
+    cout << "Ah, an excellent choice!" << endl;
+    cout << "But beware, Trainer," << endl;
+    cout << "this is only the beginning." << endl;
+    cout << "Your journey is about to unfold." << endl;
+    cout << "Now let’s see if you’ve got what it takes to keep going!" << endl;
+    cout << "Good luck, and remember… Choose wisely!"<< endl;
+
 
     return 0;
 }
